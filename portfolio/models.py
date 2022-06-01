91609c29342177c749d6c2aab9c985a2bfacc133
Robert Beazley <robertbeazley18@gmail.com>
2022-06-01 19:19:53 -0400

    launch site

--- portfolio/models.py
+++ portfolio/models.py
@@ -11,6 +11,7 @@ class Projects(models.Model):
     header_image = models.ImageField(null=True, blank=True, upload_to='images/projects')
     author = models.CharField(max_length=200,)
     pub_date = models.DateTimeField(auto_now_add=True)
+    snippet = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=False)
     website_url = models.CharField(max_length=200, null=True, blank=True)
 
