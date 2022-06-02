a95e9a20abc9e5ccf5a37c886748308483dabd31
Robert Beazley <robertbeazley18@gmail.com>
2022-06-02 17:24:38 -0400

    prelaunch checklist

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0t4g*j@5e1%%p*3msi^$c8e0akuskiqqb02jzym98(k_!y=#vp'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
