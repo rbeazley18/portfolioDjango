782242d35a21a684271f77a55ec6efab3d216dab
Robert Beazley <robertbeazley18@gmail.com>
2022-06-04 08:53:37 -0400

    mod on user settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,9 +90,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
-
     }
 }
 
