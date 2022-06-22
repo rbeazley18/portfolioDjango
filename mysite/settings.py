d012cb2b5d22eefe0d54c03d7baef95988c7dafc
Robert Beazley <robertbeazley18@gmail.com>
2022-06-22 18:18:45 -0400

    debug false and update resume

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['beazleyportfolio.herokuapp.com']
 
