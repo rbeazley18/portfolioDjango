e1ec0842dd24d8a8f8167b718ae3b78829718a10
Robert Beazley <robertbeazley18@gmail.com>
2022-06-24 16:46:33 -0400

    debug propogate

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['beazleyportfolio.herokuapp.com']
 
