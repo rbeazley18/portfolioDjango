7b495f1d43e7c3f02d745c50cbdeb369f11150aa
Robert Beazley <robertbeazley18@gmail.com>
2022-06-24 17:01:11 -0400

    remove debug prop

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['beazleyportfolio.herokuapp.com']
 
