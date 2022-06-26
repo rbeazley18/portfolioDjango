e8b50a7c3545c5bb005729e5871ef30f2f586311
Robert Beazley <robertbeazley18@gmail.com>
2022-06-26 08:24:20 -0400

    add portfolio url

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
-    path('', include('portfolio.urls')),
+    path('portfolio/', include('portfolio.urls')),
     path('todolist/', include('todo_app.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
