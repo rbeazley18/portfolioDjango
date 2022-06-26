870e9f9f0586808d10fed17a7584c5333e834751
Robert Beazley <robertbeazley18@gmail.com>
2022-06-26 11:11:16 -0400

    collect static

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CSRF_COOKIE_SECURE = True
