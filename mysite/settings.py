4afa941d41b2136d8dbc20b754f3d8da9757eeb3
Robert Beazley <robertbeazley18@gmail.com>
2022-06-28 19:22:41 -0400

    email setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,8 +46,8 @@ ALLOWED_HOSTS = ['beazleyportfolio.herokuapp.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = env('EMAIL_USER') #my gmail username
-EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD') #my gmail password
+EMAIL_HOST_USER = env('EMAIL_USER') 
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
